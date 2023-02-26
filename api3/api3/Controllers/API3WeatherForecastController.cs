ed2dc5b5ca06622d655624ec527c0d27cd8bea59
Surya Nandury <snandury@regionalmanagement.com>
2023-02-26 00:58:11 -0500

    Testing

--- api3/api3/Controllers/API3WeatherForecastController.cs
+++ api3/api3/Controllers/API3WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace api3.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Comfortable"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Comfortable", "Test1"
     };
 
         private readonly ILogger<API3WeatherForecastController> _logger;
