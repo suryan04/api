863f314fe46891ada54abf8c7e3b03c6a3d844b7
Surya Nandury <snandury@regionalmanagement.com>
2023-02-25 22:46:00 -0500

    added api1

---
+++ api1/api1/Controllers/API1WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class API1WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<API1WeatherForecastController> _logger;
+
+        public API1WeatherForecastController(ILogger<API1WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
